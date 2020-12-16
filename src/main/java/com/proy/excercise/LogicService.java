b25e2d07a04a775f82972c8ad77efcd5fd218f46
Gabriel <gabriel.leoni.um@gmail.com>
2020-12-15 22:58:18 -0300

    New versión
    All the paramethers are mannaged by the application, the same for the key and token.

---
+++ src/main/java/com/proy/excercise/LogicService.java
@@ -0,0 +1,43 @@
+package com.proy.excercise;
+
+import java.util.Random;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LogicService {
+	
+	public String getNumString() {
+		
+        String SALTCHARS = "1234567890";
+        StringBuilder salt = new StringBuilder();
+        Random rnd = new Random();
+        while (salt.length() < 5) { // length of the random string.
+        	
+            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
+            salt.append(SALTCHARS.charAt(index));
+            
+        }
+        String saltStr = salt.toString();
+        
+        return saltStr;
+
+    }
+	
+	public String getWordString() {
+		
+        String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        StringBuilder salt = new StringBuilder();
+        Random rnd = new Random();
+        while (salt.length() < 5) { // length of the random string.
+        	
+            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
+            salt.append(SALTCHARS.charAt(index));
+            
+        }
+        String saltStr = salt.toString();
+        
+        return saltStr;
+
+    }
+}
--- src/main/java/com/proy/excercise/controller/ApiController.java
+++ src/main/java/com/proy/excercise/controller/ApiController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
+import com.proy.excercise.LogicService;
+
 
 @RestController
 @RequestMapping("/api")
@@ -20,33 +22,94 @@ public class ApiController {
 	
 	@Autowired
 	private Environment env;
+	
+	@Autowired
+	private LogicService logic;
 
 
 //	@SuppressWarnings("unchecked")
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public HttpStatus createTask(
-			@RequestParam String key,
-			@RequestParam String token,
-			@RequestParam String idList,
-			@RequestParam String name,
-			@RequestParam (required = false) String desc,
+			@RequestParam (required = false)String key,
+			@RequestParam (required = false)String token,
+			@RequestParam (required = false)String idList,
+			@RequestParam String title,
+			@RequestParam String type,
+			@RequestParam (required = false) String description,
+			@RequestParam (required = false) String category,
 			@RequestParam (required = false) String due) {
 		
+		//USABLES: type, title, description, category
+		
 		String urlTrello = env.getProperty("trelloCardUrl");
+		key = env.getProperty("TrelloKey");
+		token = env.getProperty("TrelloToken");
+		
+		String issue = "issue";
+		String bug = "bug";
+		String task ="task";
+		
+		String mant = "Maintenance";
+		String res = "Research";
+		String test = "Test";
+		
+		
+		String url = urlTrello+"?key=" + key + "&token=" + token;
+		
+		
+		/**
+		 * In case of a task, the card will be assigned to the list depending on the category.
+		 * If the category doesn't match it will return 404 Not Found.
+		 */
+		if(task.equalsIgnoreCase(type)) {
+			if(mant.equalsIgnoreCase(category)) {
+				
+				idList = env.getProperty("MaintenanceListId");
+				
+			}else if(res.equalsIgnoreCase(category)) {
+				
+				idList = env.getProperty("ResearchListId");
+				
+			}else if(test.equalsIgnoreCase(category)) {
+				
+				idList = env.getProperty("TestListId");
+				
+			}else {
+				return HttpStatus.NOT_FOUND;
+			}
+		}
+			
+		if(bug.equalsIgnoreCase(type)) {
+			title.concat("-"+logic.getWordString()+"-"+logic.getNumString());
+			title.concat("&idLabels=" + env.getProperty("BugLabelId"));
+			idList = env.getProperty("ToDoListId");
+			url.concat("&idList=" + idList);
+		}
+		
+		if(issue.equalsIgnoreCase(type)) {
+			
+			url = url.concat("&idList=" + env.getProperty("ToDoListId") + "&name=" + title);
+			
+		}
+		
+		
+		
 		
 		try {
 			
 			RestTemplate rt = new RestTemplate();
 			
-			String url = urlTrello+"?key="+key+"&token="+token+"&idList="+idList+"&name="+name+"&desc="+desc+"&due="+due;
+			//String url = urlTrello+"?key=" + key + "&token=" + token + "&idList=" + idList + "&name=" + title + "&desc="+ description + "&due=" + due;
 			
 			rt.postForLocation(url, null);
 			
 		} catch (RestClientException e) {
 			e.printStackTrace();
+			System.out.println("******************************************"+url);
 		}
 		
+		System.out.println("******************************************"+url);
 		return HttpStatus.OK;
 	}
 	
